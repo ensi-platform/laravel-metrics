eb6873eae5cd56cd801846b13c2925bffbce0621
Ms_Natali <golubkova@greensight.ru>
2023-08-24 14:09:07 +0300

    ECS-310

--- src/Guzzle/GuzzleMiddleware.php
+++ src/Guzzle/GuzzleMiddleware.php
@@ -11,22 +11,22 @@ use function resolve;
 
 class GuzzleMiddleware
 {
-    public static function middleware()
+    public static function middleware(string $type = 'http_client')
     {
-        return function(callable $handler) {
-            return static function(RequestInterface $request, array $options) use ($handler) {
+        return function(callable $handler) use ($type) {
+            return static function(RequestInterface $request, array $options) use ($type, $handler) {
                 $start = microtime(true);
                 $response = $handler($request, $options);
                 if ($response instanceof PromiseInterface) {
-                    return $response->then(function ($result) use ($start, $request) {
-                        self::handleResponse($start, $request);
+                    return $response->then(function ($result) use ($type, $start, $request) {
+                        self::handleResponse($type, $start, $request->getHeaderLine('host'));
                         return $result;
-                    })->otherwise(function ($reason) use ($start, $request) {
-                        self::handleResponse($start, $request);
+                    })->otherwise(function ($reason) use ($type, $start, $request) {
+                        self::handleResponse($type, $start, $request->getHeaderLine('host'));
                         return new RejectedPromise($reason);
                     });
                 } else {
-                    self::handleResponse($start, $request);
+                    self::handleResponse($type, $start, $request->getHeaderLine('host'));
                 }
 
                 return $response;
@@ -34,20 +34,16 @@ class GuzzleMiddleware
         };
     }
 
-    /**
-     * @param $start
-     * @param RequestInterface $request
-     */
-    public static function handleResponse($start, $request): void
+    public static function handleResponse(string $type, $start, string $host): void
     {
         $end = microtime(true);
 
         /** @var LatencyProfiler $profiler */
         $profiler = resolve(LatencyProfiler::class);
-        $profiler->addAsyncTimeQuant('http_client', $start, $end);
+        $profiler->addAsyncTimeQuant($type, $start, $end);
 
         Prometheus::update('http_client_seconds_total', $end - $start, [
-            'host' => $request->getHeaderLine('host')
+            'host' => $host
         ]);
     }
 }
