24c4d44f64f65a46a30af7b620601a80f201f26f
Kapitanov Andrey <kapitanov@greensight.ru>
2023-09-07 15:52:38 +0300

    ECS-311

---
+++ tests/Factories/CustomJob.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Ensi\LaravelMetrics\Tests\Factories;
+
+use Illuminate\Contracts\Queue\Job as JobInterface;
+use Illuminate\Queue\Jobs\Job as QueueJob;
+use Faker\Factory;
+
+class CustomJob extends QueueJob implements JobInterface
+{
+    public string $connection;
+
+    public function __construct(public $connectionName, public $queue, public $attempts = 1)
+    {
+        $this->connection = $this->connectionName;
+    }
+
+    public static function factory(): self
+    {
+        $faker = Factory::create();
+
+        return new CustomJob(
+            connectionName: $faker->word(),
+            queue: $faker->word(),
+            attempts: $faker->numberBetween(0, 10),
+        );
+    }
+
+    public function getJobId(): string
+    {
+        return 'default';
+    }
+
+    public function getRawBody(): string
+    {
+        return '';
+    }
+
+    public function attempts(): int
+    {
+        return $this->attempts;
+    }
+}
